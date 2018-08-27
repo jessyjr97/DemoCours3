a15ca20119a3efe73244600b0bd8f63e1c9d1b6f
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2018-08-27 16:55:45 -0400

    Création d'un joueur avec un gun

---
+++ Assets/Scripts/FireGun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour {
+    [SerializeField] GameObject bullet;
+    [SerializeField] float fireRate = 1;
+    [SerializeField] GameObject bulletEmitter;
+    // Use this for initialization
+    private float timerLastShot = 1;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerLastShot += Time.deltaTime;
+        var fire1 = Input.GetAxis("Fire1");
+        if(fire1 > 0 && timerLastShot > fireRate)
+        {
+            Instantiate(bullet, bulletEmitter.transform.position, bulletEmitter.transform.rotation);
+            timerLastShot = 0;
+        }
+	}
+}
